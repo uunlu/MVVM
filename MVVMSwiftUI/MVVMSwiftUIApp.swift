93fa7d184574e15a292600f31047eb2d9fc3b8fc
Ugur Unlu <vally10@live.com>
2022-02-10 11:13:27 +0100

    initial commit

---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Ugur Unlu on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
